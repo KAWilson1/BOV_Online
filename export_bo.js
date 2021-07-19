28aef9d67c66a936de1aae7a21f3b5ecec04c0df
Kyle Wilson <kylewilson10@gmail.com>
2021-07-19 12:36:26 -0400

    Added export button to delete desired columns

---
+++ export_bo.js
@@ -0,0 +1,34 @@
+function export_bo(){
+    /*
+        Deletes "Delete" and "Insert Below" Headers
+        Deletes "Delete" and "Insert Below" sections within bo_units
+    */
+    var bo_container = document.querySelector('#bo_container');
+
+    // For every bo_unit, delete "Delete" and "Insert Below" columns
+    var bo_units = bo_container.querySelectorAll('div .bo_unit');
+    for(var i = 0; i < bo_units.length; i++){
+        var curr_row = bo_units[i].querySelector('.row');
+        try{
+            var to_delete;
+            to_delete = curr_row.querySelector(".delete");
+            to_delete.remove();
+            to_delete = curr_row.querySelector(".insert");
+            to_delete.remove();
+        }
+        catch(err){
+            console.log(err.message);
+        } 
+    }
+
+    // Remove "Delete" and "Insert Below" headers
+    var bo_headers = bo_container.querySelector("#bo_headers");
+    var bo_header_row = bo_headers.querySelector(".row");
+    // Since "Delete" and "Insert Below" are the last columns,
+    // delete the last child in the row twice
+    bo_header_row.removeChild(bo_header_row.lastElementChild);
+    bo_header_row.removeChild(bo_header_row.lastElementChild);
+
+
+
+}
