263210a3a7181db827d976fc909bca4bb7a1681a
Kyle Wilson <kylewilson10@gmail.com>
2021-08-04 12:00:55 -0400

    Enhanced stop editing by dynamically updating bo_container

--- js/export_bo.js
+++ js/export_bo.js
@@ -10,33 +10,41 @@ function stop_editing(){
     */
     var bo_container = document.querySelector('#bo_container');
 
+    
     // For every bo_unit, hide "Delete" and "Insert Below" columns
+    // Widen Notes column to take up the new space
     var bo_units = bo_container.querySelectorAll('div .bo_unit');
     for(var i = 0; i < bo_units.length; i++){
         var curr_row = bo_units[i].querySelector('.row');
         try{
+            // Delete columns
             var to_delete;
             to_delete = curr_row.querySelector(".delete");
             to_delete.style.display = "none";
             to_delete = curr_row.querySelector(".insert");
             to_delete.style.display = "none";
+            // Widen notes
+            var notes_step = curr_row.querySelector(".note_step");
+            notes_step.classList.remove("col-4");
+            notes_step.classList.add("col-6");
         }
         catch(err){
             console.log(err.message);
         } 
     }
+    
 
-    /*
-    Commented code until decision is made if "Delete" and "Insert" columns are deleted or hidden
-    // Remove "Delete" and "Insert Below" headers
+    
+    // Remove "Delete" and "Insert Below" headers and widen Notes Header
     var bo_headers = bo_container.querySelector("#bo_headers");
     var bo_header_row = bo_headers.querySelector(".row");
+    bo_header_row.removeChild(bo_header_row.lastElementChild);
+    bo_header_row.removeChild(bo_header_row.lastElementChild);
 
-    // Since "Delete" and "Insert Below" are the last columns,
-    // delete the last child in the row twice
-    bo_header_row.removeChild(bo_header_row.lastElementChild);
-    bo_header_row.removeChild(bo_header_row.lastElementChild);
-    */
+    var notes_header = bo_container.querySelector("#bo_header_notes");
+    notes_header.classList.remove("col-4");
+    notes_header.classList.add("col-6");
+    
     
 
     // Hide Action Selector
@@ -60,22 +68,60 @@ function stop_editing(){
 }
 
 function start_editing(){
+    var bo_container = document.querySelector('#bo_container');
+    
     //Hide Start Editing Button
     var start_editing = document.getElementById("start_editing_menu");
     start_editing.style.display = "none";
 
+    // Add "Delete" and "Insert" headers; make notes header more narrow
+    var bo_headers = bo_container.querySelector("#bo_headers");
+    var bo_header_row = bo_headers.querySelector(".row");
     
-    var bo_container = document.querySelector('#bo_container');
-    // For every bo_unit, hide "Delete" and "Insert Below" columns
+    // Create delete and insert headers and append them
+    var div_delete = document.createElement('div');
+    div_delete.setAttribute("class", "col-1 bo_header");
+    div_delete.setAttribute("id", "bo_header_delete");
+    var center_delete = document.createElement('center');
+    center_delete.innerHTML = "Delete";
+    div_delete.appendChild(center_delete);
+
+    var div_insert = document.createElement('div');
+    div_insert.setAttribute("class", "col-1 bo_header");
+    div_insert.setAttribute("id", "bo_header_insert");
+    var center_insert = document.createElement('center');
+    center_insert.innerHTML = "Insert";
+    div_insert.appendChild(center_insert);
+
+    bo_header_row.appendChild(div_delete);
+    bo_header_row.appendChild(div_insert);
+
+    var notes_header = bo_container.querySelector("#bo_header_notes");
+    notes_header.classList.remove("col-6");
+    notes_header.classList.add("col-4");
+    
+
+
+
+
+
+    
+    // For every bo_unit, display "Delete" and "Insert Below" columns
     var bo_units = bo_container.querySelectorAll('div .bo_unit');
     for(var i = 0; i < bo_units.length; i++){
         var curr_row = bo_units[i].querySelector('.row');
         try{
-            var to_delete;
-            to_delete = curr_row.querySelector(".delete");
-            to_delete.style.display = "block";
-            to_delete = curr_row.querySelector(".insert");
-            to_delete.style.display = "block";
+            // Show delete and insert icons
+            var to_display;
+            to_display = curr_row.querySelector(".delete");
+            to_display.style.display = "block";
+            to_display = curr_row.querySelector(".insert");
+            to_display.style.display = "block";
+
+            // Make the notes column more narrow to accommodate for insert/delete
+            var notes_step = curr_row.querySelector(".note_step");
+            notes_step.classList.remove("col-6");
+            notes_step.classList.add("col-4");
         }
         catch(err){
             console.log(err.message);
